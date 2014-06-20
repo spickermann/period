d79899085f4a31d576e38a8461402976d29b3e8c
Martin Spickermann <spickermann@gmail.com>
2014-06-20 22:38:42 +1000

    specs centralized method to get the current time

---
+++ spec/period_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Period::Year do
+  describe '.current_time' do
+    let(:time) { Time.parse('1989-02-12 15:10:00') }
+
+    before do
+      allow(Time).to receive(:now).and_return(time)
+    end
+
+    it 'returns the current time' do
+      expect(Period.current_time).to eq (time)
+    end
+  end
+end
